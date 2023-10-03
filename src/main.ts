5dcee6fbbca93d40cf175599875c6ee82fd60ed1
Hampton Paulk <email@hamptonpaulk.com>
2023-10-03 14:20:34 -0400

    add zone import for stackblitz issues

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'zone.js/dist/zone'; // this is used to deal with stackblitz issues
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { bootstrapApplication } from '@angular/platform-browser';
